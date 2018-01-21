2b63aab8da85159757f71541101a5721809c2e82
Marc Choisy <marc.choisy@ird.fr>
2018-01-21 17:07:23 +0700

    Adding function download

---
+++ R/download.R
@@ -0,0 +1,28 @@
+#' Download a package file from an URL
+#'
+#' In case a raw data file is too big to be contained in a package, a solution
+#' consists in downloading that file after package installation when the file is
+#' needed for the first time. This is what the download function allows to do.
+#' It will downlaod the file from the provided URL and put it in the extdata
+#' directory of the provided package hierarchy, naming it using the name
+#' provided as an argument.
+#'
+#' @param url character string containing the URL to download from.
+#'
+#' @param package character string containing the name of the package into which
+#'                to put the downloaded file.
+#'
+#' @param file character string containing the name of the downloaded file
+#'             should be given.
+#'
+#' @export
+#'
+#' @author Marc Choisy
+#'
+download <- function(url, package, file) {
+  path <- path.package(package)
+  destdir <- paste0(path, "/extdata")
+  if (!dir.exists(destdir)) dir.create(destdir)
+  download.file(url, paste0(destdir, "/", file))
+  invisible(url)
+}
