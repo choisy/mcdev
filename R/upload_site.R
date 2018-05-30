09c3af165616b5f8d11b2c3df16a10108bc654e1
Marc Choisy <marc.choisy@ird.fr>
2018-05-30 14:19:06 +0700

    Adding the upload_site function as a replacement of the buildsite function

---
+++ R/upload_site.R
@@ -0,0 +1,26 @@
+#' Upload a website
+#'
+#' It uses \code{\link[mcdev]{publishdir}} to retrieve the directory of the
+#' website, then, from the website directory, it uses git commands to (i) stage
+#' all new changes, (ii) commit these change (together with the input argument
+#' message), and (iii) push upstream (which is uploading).
+#'
+#' The combination of `pkgdown::build_site` then `mcdev::upload_site` is
+#' equivalent to `mcdev::buildsite` but we recommand to use the former option
+#' instead.
+#'
+#' @param message character string containing the message of the git commit.
+#'
+#' @importFrom blogdown build_site
+#'
+#' @export
+#'
+#' @author Marc Choisy
+upload_site <- function(message = "update") {
+  build_site()
+  system(paste("cd", publishdir(),
+               "; git add -A",
+               "; git commit -m \"", message, "\"",
+               "; git push"))
+  invisible(0)
+}
