b0b5e6f27383893c739738b219270e6f2c663692
Marc Choisy <marc.choisy@ird.fr>
2018-11-21 18:07:30 +0700

    Fixing bug in download

--- R/download.R
+++ R/download.R
@@ -27,6 +27,6 @@ download <- function(url, package, file, ...) {
   path <- find.package(package)
   destdir <- paste0(path, "/extdata")
   if (!dir.exists(destdir)) dir.create(destdir)
-  utils::download.file(url, paste0(destdir, "/", file, ...))
+  utils::download.file(url, paste0(destdir, "/", file), ...)
   invisible(url)
 }
